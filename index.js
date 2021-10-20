f6eb8534f3ac57ddb4994f3a8327ac4834b324a7
Animesh <animeshk55@gmail.com>
2021-10-20 07:53:43 +0530

    1

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+fasdf
+dsf
+SharedArrayBufferdf
+ds
+FormDatasaf
+dsfdsf
+ds
+f
